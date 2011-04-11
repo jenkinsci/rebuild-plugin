73d1aff8e24ad6a332f986a275ba39f55511f9a6
Shemeer Sulaiman <shemeer.x.sulaiman@sonyericsson.com>
2011-04-11 15:06:46 +0900

    Backward compatibility implemented
    Some transient variables are introduced to maintain
    the backward compatiability of the plugin.

---
+++ src/main/java/com/sonyericsson/hudson/plugins/rebuild/Root.java
@@ -0,0 +1,12 @@
+package com.sonyericsson.hudson.plugins.rebuild;
+
+import com.sonyericsson.rebuild.RebuildAction;
+/**
+ * Root class.
+ * This class is declared only for
+ * backward compatibility.
+ *
+ * @author Shemeer S;
+ */
+public class Root extends RebuildAction {
+}
