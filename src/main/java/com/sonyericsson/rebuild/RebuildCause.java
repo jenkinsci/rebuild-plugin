e9a724323f84fff12292576108f7409158832892
Oleg Nenashev <nenashev@synopsys.com>
2013-09-09 14:28:13 +0400

    Added license notes to RebuildCause.java
    Signed off by Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/com/sonyericsson/rebuild/RebuildCause.java
+++ src/main/java/com/sonyericsson/rebuild/RebuildCause.java
@@ -1,18 +1,41 @@
+/*
+ *  The MIT License
+ *
+ *  Copyright 2013 Joel Johnson and contributors.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 package com.sonyericsson.rebuild;
 
 import hudson.model.Cause;
 import hudson.model.Run;
 
 /**
- * A cause specifying that the build was a rebuild of another build.
- * Extends UpstreamCause so that a lot of the magic that Jenkins does with Upstream builds is inherited (linking, etc).
+ * A cause specifying that the build was a rebuild of another build. Extends
+ * UpstreamCause so that a lot of the magic that Jenkins does with Upstream
+ * builds is inherited (linking, etc).
  *
- * User: Joel Johnson
- * Date: 1/30/13
- * Time: 2:23 PM
+ * @author Joel Johnson
  */
 public class RebuildCause extends Cause.UpstreamCause {
-	public RebuildCause(Run<?, ?> up) {
-		super(up);
-	}
+
+    public RebuildCause(Run<?, ?> up) {
+        super(up);
+    }
 }
