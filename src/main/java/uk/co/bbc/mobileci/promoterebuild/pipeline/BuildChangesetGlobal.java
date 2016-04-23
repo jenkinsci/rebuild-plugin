243c6d3a8f7026c78576a111ff094095de731f2a
beazlr02 <ross.beazley@bbc.co.uk>
2016-04-23 22:16:28 +0100

    a wholly untested change log generator based on https://issues.jenkins-ci.org/browse/JENKINS-30412

---
+++ src/main/java/uk/co/bbc/mobileci/promoterebuild/pipeline/BuildChangesetGlobal.java
@@ -0,0 +1,69 @@
+package uk.co.bbc.mobileci.promoterebuild.pipeline;
+
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.scm.ChangeLogSet;
+import org.jenkinsci.plugins.workflow.cps.CpsScript;
+import org.jenkinsci.plugins.workflow.cps.GlobalVariable;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+
+import java.util.List;
+
+/**
+ * Created by beazlr02 on 23/04/16.
+ */
+@Extension
+public class BuildChangesetGlobal extends GlobalVariable {
+
+
+    @Override
+    public String getName() {
+        return "pipelineBuildChangeset";
+    }
+
+    @Override
+    public Object getValue(CpsScript script) throws Exception {
+        Run<?, ?> build = script.$build();
+        if (build == null) {
+            throw new IllegalStateException("cannot find associated build");
+        }
+
+        WorkflowRun workflowRun = null;
+        if (build instanceof WorkflowRun) {
+            workflowRun = (WorkflowRun) build;
+        }
+
+        return new BuildChangeSet(workflowRun);
+    }
+
+    public static final class BuildChangeSet {
+
+        private WorkflowRun workflowRun;
+
+        public BuildChangeSet(WorkflowRun result) {
+            this.workflowRun = result;
+        }
+
+        public String getChangeSet() {
+            StringBuilder changeSet = new StringBuilder();
+
+            if (workflowRun != null) {
+                for (ChangeLogSet<? extends ChangeLogSet.Entry> entries : workflowRun.getChangeSets()) {
+
+                    for (ChangeLogSet.Entry entry : entries) {
+
+                        changeSet.append(entry.getCommitId())
+                                .append(" ")
+                                .append(entry.getMsg())
+                                .append("\n");
+                    }
+                }
+            }
+
+            return changeSet.toString();
+        }
+    }
+
+}
+
+
