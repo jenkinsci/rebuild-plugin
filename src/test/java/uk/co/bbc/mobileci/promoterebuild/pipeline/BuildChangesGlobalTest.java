f286ca643793c028c6c2efd878fe6b76e42a1737
beazlr02 <ross.beazley@bbc.co.uk>
2016-04-24 19:10:29 +0100

    adds support for testing with git repos, including a smoke test

--- src/test/java/uk/co/bbc/mobileci/promoterebuild/pipeline/BuildChangesGlobalTest.java
+++ src/test/java/uk/co/bbc/mobileci/promoterebuild/pipeline/BuildChangesGlobalTest.java
@@ -4,12 +4,19 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.console.AnnotatedLargeText;
 import hudson.model.queue.QueueTaskFuture;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.scm.GitSampleRepoRule;
+import org.jenkinsci.plugins.workflow.steps.scm.GitStep;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.model.Statement;
 import org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildAction;
+import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RestartableJenkinsRule;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -19,23 +26,45 @@ import static org.junit.Assert.assertNull;
  */
 public class BuildChangesGlobalTest {
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+    @ClassRule
+    public static BuildWatcher buildWatcher = new BuildWatcher();
+
+    @Rule public JenkinsRule story = new JenkinsRule();
+
+    @Rule public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
 
     @Test
     public void promotedJobGlobalCreated() throws Exception {
-        WorkflowJob p = j.jenkins.createProject(WorkflowJob.class, "p");
-        p.setDefinition(new CpsFlowDefinition(
-                "node {\n" +
-                        "  echo pipelineBuildChangeset.getChangeSet()\n" +
-                        "}", true));
 
-        j.assertBuildStatusSuccess(p.scheduleBuild2(0));
+                WorkflowJob p = story.jenkins.createProject(WorkflowJob.class, "p");
+                p.setDefinition(new CpsFlowDefinition(
+                        "node {\n" +
+                                "  echo pipelineBuildChangeset.getChangeSet()\n" +
+                                "}", true));
 
-        //dump a log for debugging
-        //AnnotatedLargeText logText = p.getLastCompletedBuild().getLogText();
-        //logText.writeRawLogTo(0, System.err);
+                story.assertBuildStatusSuccess(p.scheduleBuild2(0));
+
+
     }
 
 
+
+    @Test public void smokeTests() throws Exception {
+
+        sampleRepo.init();
+        String script = "node {\n" +
+                "  echo pipelineBuildChangeset.getChangeSet()\n" +
+                "  echo 'A_MSG_IN_LOG'\n" +
+                "}";
+        sampleRepo.write("Jenkinsfile", script);
+        sampleRepo.git("add", "Jenkinsfile");
+        sampleRepo.git("commit", "--message=files");
+
+        WorkflowJob p = story.jenkins.createProject(WorkflowJob.class, "p");
+        p.setDefinition(new CpsScmFlowDefinition(new GitStep(sampleRepo.toString()).createSCM(), "Jenkinsfile"));
+        WorkflowRun b = p.scheduleBuild2(0).waitForStart();
+        story.assertLogContains("A_MSG_IN_LOG",
+                story.assertBuildStatusSuccess(story.waitForCompletion(b)));
+
+    }
 }
