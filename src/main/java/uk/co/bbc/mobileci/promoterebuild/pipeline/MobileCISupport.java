7c8fb6561e4f2473088a84420857e6ad4a40500f
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-09-13 13:59:12 +0100

    Decorate methods with @NonCPS to indicate that methods do not rely on serialisation
    See https://github.com/jenkinsci/workflow-cps-plugin/blob/master/README.md#technical-design

--- src/main/java/uk/co/bbc/mobileci/promoterebuild/pipeline/MobileCISupport.java
+++ src/main/java/uk/co/bbc/mobileci/promoterebuild/pipeline/MobileCISupport.java
@@ -1,5 +1,6 @@
 package uk.co.bbc.mobileci.promoterebuild.pipeline;
 
+import com.cloudbees.groovy.cps.NonCPS;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
 
 import java.io.IOException;
@@ -18,26 +19,31 @@ public final class MobileCISupport {
         this.kvStoreProxy = kvStoreProxy;
     }
 
+    @NonCPS
     @Whitelisted
     public boolean getPromotion() {
         return promotedJob.isPromotion();
     }
 
+    @NonCPS
     @Whitelisted
     public boolean isPromotion() {
         return promotedJob.isPromotion();
     }
 
+    @NonCPS
     @Whitelisted
     public String getFromHash() {
         return promotedJob.getHash();
     }
 
+    @NonCPS
     @Whitelisted
     public String getFromBuildNumber() {
         return promotedJob.getFromBuildNumber();
     }
 
+    @NonCPS
     @Whitelisted
     public String getBuildTriggerHash() {
         return buildChangeSet.getBuildTriggerHash();
@@ -47,26 +53,31 @@ public final class MobileCISupport {
         return "PromotedJob: from: " +getFromBuildNumber() + " for:"+getFromHash();
     }
 
+    @NonCPS
     @Whitelisted
     public void store(String key, String value) throws IOException {
         kvStoreProxy.store(key,value);
     }
 
+    @NonCPS
     @Whitelisted
     public String retrieve(String key) {
         return kvStoreProxy.retrieve(key);
     }
 
+    @NonCPS
     @Whitelisted
     public String getChangeSet() {
         return buildChangeSet.getChangeSet();
     }
 
+    @NonCPS
     @Whitelisted
     public String getBranchName() {
         return buildChangeSet.getBranchName();
     }
 
+    @NonCPS
     @Whitelisted
     public Collection<String> getBranchNames() {
         return buildChangeSet.getBranchNames();
